507ff1d4033b23b0238397ab54c96ee67f29f7f3
panjinshuai <2269845200@qq.com>
2021-04-12 16:39:34 +0800

    提交index代码

--- assets/js/baseApi.js
+++ assets/js/baseApi.js
@@ -1,4 +1,23 @@
 $.ajaxPrefilter(function(options) {
-    console.log(options.url);
+    // console.log(options.url);
+    // 拼接url
     options.url = 'http://api-breakingnews-web.itheima.net' + options.url;
+
+    // 统一为有权限的接口，设置 headers 请求头
+    if (options.url.includes('/my/')) {
+        options.headers = {
+            Authorization: localStorage.token || '',
+        };
+    }
+
+    // 全局统一挂载 complete 回调函数
+    options.complete = function(response) {
+        console.log(response);
+        const res = response.responseJSON;
+        if (res.status === 1 && res.message === '身份认证失败！') {
+            // 说明没有权限
+            localStorage.removeItem('token');
+            location.href = '/login.html';
+        }
+    }
 })
